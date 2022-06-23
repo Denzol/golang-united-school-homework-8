06a4d2170e3c4f9749c2cfdef411ea1d419d2dec
Hramtsov Denis <denis.khramtsov@mail.ru>
2022-06-23 19:36:13 +1000

    vrsion 4

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -10,145 +11,153 @@ import (
 	"os"
 )
 
+const (
+	opAdd      = "add"
+	opList     = "list"
+	opRemove   = "remove"
+	opFindByID = "findById"
+)
+
 type Arguments map[string]string
 
 type User struct {
-	Id    string `json:"id"`
+	ID    string `json:"id"`
 	Email string `json:"email"`
 	Age   int    `json:"age"`
 }
 
-type List struct {
-	Users []User
+func Perform(args Arguments, writer io.Writer) (err error) {
+
+	if err = checkArguments(args); err != nil {
+		return
+	}
+	switch args["operation"] {
+	case opList:
+		var dataIn []byte
+		if dataIn, err = ioutil.ReadFile(args["fileName"]); err != nil {
+			log.Fatal("Cannot load settings:", err)
+		}
+		if _, err = writer.Write(dataIn); err != nil {
+			return
+		}
+	case opAdd:
+		if err = doAdd(args["fileName"], args["item"], writer); err != nil {
+			log.Fatal(err)
+		}
+	case opRemove:
+		if err = doRemove(args["fileName"], args["id"], writer); err != nil {
+			log.Fatal(err)
+		}
+	case opFindByID:
+		if err = doFindByID(args["fileName"], args["id"], writer); err != nil {
+			log.Fatal(err)
+		}
+	default:
+		err = fmt.Errorf("Operation %s not allowed!", args["operation"])
+	}
+	return
 }
 
-func Perform(args Arguments, writer io.Writer) error {
-	const k = "flag has to be specified"
+func doFindByID(fileName, id string, writer io.Writer) (err error) {
+
+	var users []User
+	if users, err = loadUsers(fileName); err != nil {
+		return
+	}
+	for _, user := range users {
+		if user.ID == id {
+			var dataOut []byte
+			if dataOut, err = json.Marshal(user); err != nil {
+				return fmt.Errorf("JSON marshaling failed: %s", err.Error())
+			}
+			_, err = writer.Write(dataOut)
+			break
+		}
+	}
+	return
+}
+
+func doRemove(fileName, id string, writer io.Writer) (err error) {
+
+	var users []User
+	if users, err = loadUsers(fileName); err != nil {
+		return
+	}
+	for idx, user := range users {
+		if user.ID == id {
+			var fileData []byte
+			users = append(users[:idx], users[idx+1:]...)
+			if fileData, err = json.Marshal(&users); err != nil {
+				return fmt.Errorf("JSON marshaling failed: %s", err.Error())
+			}
+			if err = ioutil.WriteFile(fileName, fileData, 0600); err != nil {
+				return fmt.Errorf("Cannot write updated settings file: %s", err.Error())
+			}
+			break
+		}
+	}
+	_, err = writer.Write([]byte(fmt.Sprintf("Item with id %s not found", id)))
+	return
+}
+
+func doAdd(fileName, item string, writer io.Writer) (err error) {
+
+	var users []User
+	if users, err = loadUsers(fileName); err != nil {
+		return
+	}
+	var argUser User
+	if err = json.Unmarshal([]byte(item), &argUser); err != nil {
+		return fmt.Errorf("JSON unmarshalling failed: %s", err.Error())
+	}
+	for _, user := range users {
+		if user.ID == argUser.ID {
+			_, err = writer.Write([]byte("Item with id 1 already exists"))
+			return
+		}
+	}
+	var data []byte
+	users = append(users, argUser)
+	if data, err = json.Marshal(&users); err != nil {
+		return fmt.Errorf("JSON marshaling failed: %s", err.Error())
+	}
+	err = ioutil.WriteFile(fileName, data, 0777)
+	if err != nil {
+		err = fmt.Errorf("Cannot write updated settings file: %s", err.Error())
+	}
+	return
+}
+
+func loadUsers(fileName string) (users []User, err error) {
+
+	var data []byte
+	if data, err = ioutil.ReadFile(fileName); err == nil {
+		if err = json.Unmarshal(data, &users); err != nil {
+			err = fmt.Errorf("JSON unmarshalling failed: %s", err.Error())
+			return
+		}
+	}
+	if err != nil && errors.Is(err, os.ErrNotExist) {
+		err = fmt.Errorf("open file error: %s", err.Error())
+	}
+	return users, nil
+}
+
+func checkArguments(args Arguments) (err error) {
+
+	if args["fileName"] == "" {
+		return errors.New("-fileName flag has to be specified")
+	}
 	if args["operation"] == "" {
-		err := fmt.Errorf("-operation %s", k)
-		return err
+		return errors.New("-operation flag has to be specified")
+	}
+	if (args["operation"] == "remove" || args["operation"] == "findById") && args["id"] == "" {
+		return errors.New("-id flag has to be specified")
 	}
 	if args["item"] == "" && args["operation"] == "add" {
-		err := fmt.Errorf("-item %s", k)
-		return err
+		return errors.New("-item flag has to be specified")
 	}
-	if args["id"] == "" && args["operation"] == "remove" {
-		err := fmt.Errorf("-id %s", k)
-		return err
-	}
-	if args["id"] == "" && args["operation"] == "findById" {
-		err := fmt.Errorf("-id %s", k)
-		return err
-	}
-	if args["operation"] != "" && args["operation"] != "list" && args["operation"] != "add" && args["operation"] != "remove" && args["operation"] != "findById" {
-		err := fmt.Errorf("Operation %s not allowed!", args["operation"])
-		return err
-	}
-	if args["fileName"] == "" {
-		err := fmt.Errorf("-fileName %s", k)
-		return err
-	}
-	if args["operation"] == "list" {
-		dataIn, err := ioutil.ReadFile(args["fileName"])
-		if err != nil {
-			log.Fatal("Cannot load settings:", err)
-		}
-		writer.Write(dataIn)
-	}
-
-	if args["operation"] == "add" {
-		var luser User
-		err := json.Unmarshal([]byte(args["item"]), &luser)
-		if err != nil {
-			log.Fatal("JSON unmarshaling failed:", err)
-		}
-		file, err := os.OpenFile(args["fileName"], os.O_RDONLY|os.O_CREATE, 0777)
-		if err != nil {
-			log.Fatal("Cannot load settings:", err)
-		}
-		data, err := ioutil.ReadAll(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-		var res []User
-		err = json.Unmarshal(data, &res)
-		for i := 0; i < len(res); i++ {
-			if res[i].Id == luser.Id {
-				dataIn := "Item with id 1 already exists"
-				writer.Write([]byte(dataIn))
-			}
-		}
-		res = append(res, luser)
-		data, err = json.Marshal(&res)
-		if err != nil {
-			log.Fatal("JSON marshaling failed:", err)
-		}
-		err = ioutil.WriteFile(args["fileName"], data, 0777)
-		if err != nil {
-			log.Fatal("Cannot write updated settings file:", err)
-		}
-	}
-
-	if args["operation"] == "remove" {
-		file, err := os.OpenFile(args["fileName"], os.O_RDONLY|os.O_CREATE, 0777)
-		if err != nil {
-			log.Fatal("Cannot load settings:", err)
-		}
-		data, err := ioutil.ReadAll(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-		var res []User
-		err = json.Unmarshal(data, &res)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for i := 0; i < len(res); i++ {
-			if res[i].Id == "1" {
-				res[i] = res[len(res)-1]
-				res[len(res)-1] = User{}
-				res = res[:len(res)-1]
-				fileData, err := json.Marshal(&res)
-				if err != nil {
-					log.Fatal("JSON marshaling failed:", err)
-				}
-				err = ioutil.WriteFile(args["fileName"], fileData, 0777)
-				if err != nil {
-					log.Fatal("Cannot write updated settings file:", err)
-				}
-			}
-		}
-		writer.Write([]byte("Item with id 2 not found"))
-
-	}
-	if args["operation"] == "findById" {
-		file, err := os.OpenFile(args["fileName"], os.O_RDONLY|os.O_CREATE, 0777)
-		if err != nil {
-			log.Fatal("Cannot load settings:", err)
-		}
-		data, err := ioutil.ReadAll(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-		var res []User
-		err = json.Unmarshal(data, &res)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for i := 0; i < len(res); i++ {
-			if res[i].Id == args["id"] {
-				dataOut, err := json.Marshal(&res[i])
-				if err != nil {
-					log.Fatal("JSON marshaling failed:", err)
-				}
-				writer.Write(dataOut)
-			}
-		}
-	}
-	return nil
+	return err
 }
 
 func main() {
